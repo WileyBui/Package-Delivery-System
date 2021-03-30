113142b9b30dce239fbfc6b1d240b6bbeab1ae03
Aunya Mukherjee <mukhe074@umn.edu>
2021-03-29 20:58:37 -0500

    feature(asubject) - added getList method for testing purposes #7

--- project/tests/asubject_test.cc
+++ project/tests/asubject_test.cc
@@ -1,52 +1,39 @@
 #include "gtest/gtest.h"
 #include <EntityProject/entity.h>
-#include "../include/package.h"
 #include "../include/drone.h"
-#include "../include/customer.h"
 #include "json_helper.h"
+#include "../include/asubject.h"
+
+#include <EntityProject/web_scene_viewer.h>
+#include <EntityProject/entity_console_logger.h>
+
 #include <iostream>
 
 namespace csci3081 {
 
 using entity_project::IEntity;
 
-class DroneTest : public ::testing::Test {
+class ASubjectTest : public ::testing::Test {
  protected:
     picojson::object obj = JsonHelper::CreateJsonObject();
-    picojson::object obj2 = JsonHelper::CreateJsonObject();
     std::vector<float> position_to_add;
     std::vector<float> direction_to_add;
-    std::vector<float> position_to_add2;
-    std::vector<float> direction_to_add2;
     float radius = 1.0;
     float speed = 3.0;
-    float battery_capacity = 10000;
  public:
   virtual void SetUp() {
     JsonHelper::AddStringToJsonObject(obj, "type", "Drone");
     position_to_add.push_back(100);
     position_to_add.push_back(253.883);
     position_to_add.push_back(431.292);
-    position_to_add2.push_back(2253.883);
-    position_to_add2.push_back(4938.292);
-    position_to_add2.push_back(2533.883);
     JsonHelper::AddStdFloatVectorToJsonObject(obj, "position", position_to_add);
     JsonHelper::AddStdFloatVectorToJsonObject(obj2, "position", position_to_add2);
     direction_to_add.push_back(1);
     direction_to_add.push_back(0);
     direction_to_add.push_back(1);
-    direction_to_add2.push_back(0);
-    direction_to_add2.push_back(1);
-    direction_to_add2.push_back(0);
-    JsonHelper::AddStringToJsonObject(obj2,"name","SHINeeCD");
-    JsonHelper::AddStringToJsonObject(obj,"name","Lin");
+    JsonHelper::AddStringToJsonObject(obj,"name","Aunya");
     JsonHelper::AddStdFloatVectorToJsonObject(obj, "direction", direction_to_add);
-    JsonHelper::AddStdFloatVectorToJsonObject(obj2, "direction", direction_to_add2);
     JsonHelper::AddFloatToJsonObject(obj, "radius", radius);
-    JsonHelper::AddFloatToJsonObject(obj2, "radius", radius);
-    JsonHelper::AddFloatToJsonObject(obj2, "speed", speed);
-    JsonHelper::AddFloatToJsonObject(obj2, "battery_capacity", battery_capacity);
-    JsonHelper::AddFloatToJsonObject(obj, "battery_capacity", battery_capacity);
   }
   virtual void TearDown() {}
 };
@@ -55,6 +42,21 @@ class DroneTest : public ::testing::Test {
  * Test Cases
  ******************************************************************************/
 
-TEST_F(DroneTest, ConstructorAndGetter) {}
+TEST_F(ASubjectTest, AttachTest) {
+  IEntitySystem* entitySystem = GetEntitySystem("default");
+  IDeliverySystem* deliverySystem = dynamic_cast<IDeliverySystem*>(entitySystem);
+
+  // Create the web scene viewer
+  WebSceneViewer viewer(8081, web, scenes/all_features.json);
+  viewer.SetEntitySystem(entitySystem);
+
+  entity_project::EntityConsoleLogger logger;
+    if (deliverySystem) {
+      deliverySystem->AddObserver(&logger);
+      deliverySystem->AddObserver(&viewer);
+    }
+
+  
+}
 
 }//namespace csci3081
