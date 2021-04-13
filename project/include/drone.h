b271593c1f2f7ad3e7a695130f1b24ad6d67f4bd
Wiley Bui <17buih@gmail.com>
2021-04-13 16:19:03 -0500

    fix(drone): #38 removed the unnecessary detailed path of beeline; only needed to add 3 points instead

--- project/include/drone.h
+++ project/include/drone.h
@@ -10,13 +10,14 @@
 /*******************************************************************************
  * Includes
  ******************************************************************************/
-#include "entity_base.h"
-#include <vector>         // Used for vector like vector3D and vector2D
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>  // Used for vector like vector3D and vector2D
+
 #include "battery.h"
-#include "package.h"
 #include "carrier.h"
+#include "entity_base.h"
+#include "package.h"
 #include "vector.h"
 
 namespace csci3081 {
@@ -29,28 +30,38 @@ namespace csci3081 {
  * dynamic mode.
  */
 class Drone : public csci3081::Carrier {
-// class Drone : public csci3081::Carrier{
+  // class Drone : public csci3081::Carrier{
 
-  public:
-    /**
-      * Constructor, creates a Package object
-      * param[in] val    a picojson::object object that has the detail of the 
-      *                   package including name, position, direction, radius,
-      *                   and speed 
-    **/
-    Drone(const picojson::object& val);
+ public:
+  /**
+    * Constructor, creates a Package object
+    * param[in] val    a picojson::object object that has the detail of the 
+    *                   package including name, position, direction, radius,
+    *                   and speed 
+  **/
+  Drone(const picojson::object& val);
 
-    /**
+  /**
     * @brief Copy Constructor.
     * This creates a new instance of Package that has the same content
     * as the Package argument
     * @param[in] cpy    Drone instance that wants to be copied
     */
-    Drone(Drone&);
+  Drone(Drone&);
 
-    vector<vector<float>> GetBeelinePath(std::vector<float> src, std::vector<float> dest, float droneSpeed, float dt);
+  /**
+    * @brief Gets the beeline path (go up a certain height and fly horizontally across to the destination)
+    * This method gets the beeline path, which includes a point where the drone needs to go up to
+    * by a certain height from where it is at, then another point to the destination location with the
+    * same certain height, then another point to the final destination location.
+    * @param[in] src    Current drone location
+    * @param[in] dest   Final location where the drone needs to be
+    * 
+    * @return           The path of the beeline
+    */
+  vector<vector<float>> GetBeelinePath(std::vector<float> src, std::vector<float> dest);
 };
 
-}
+}  // namespace csci3081
 
 #endif /* INC_DRONE_H_ */
--- project/src/drone.cc
+++ project/src/drone.cc
@@ -1,9 +1,9 @@
 #include "../include/drone.h"
-#include "../include/package.h"
 #include "../include/battery.h"
-#include "../include/vector.h"
-#include "../include/json_helper.h"
 #include "../include/generate_id.h"
+#include "../include/json_helper.h"
+#include "../include/package.h"
+#include "../include/vector.h"
 
 namespace csci3081 {
 
@@ -15,8 +15,7 @@ Drone::Drone(const picojson::object& val) {
   radius = JsonHelper::GetDouble(val, "radius");
   try {
     battery = Battery(JsonHelper::GetDouble(val, "battery_capacity"));
-  }
-  catch (std::logic_error a){
+  } catch (std::logic_error a) {
     battery = Battery(10000);
   }
   dynamic = false;
@@ -27,9 +26,9 @@ Drone::Drone(const picojson::object& val) {
   type = "carrier";
 }
 
-Drone::Drone(Drone& cpy){
-  position=cpy.position;
-  direction=cpy.direction;
+Drone::Drone(Drone& cpy) {
+  position = cpy.position;
+  direction = cpy.direction;
   name = cpy.name;
   radius = cpy.radius;
   dynamic = cpy.dynamic;
@@ -42,74 +41,30 @@ Drone::Drone(Drone& cpy){
   type = "carrier";
 }
 
-vector<vector<float>> Drone::GetBeelinePath(std::vector<float> src, 
-														                std::vector<float> dest,
-														                float droneSpeed,
-														                float dt) {
+vector<vector<float>> Drone::GetBeelinePath(std::vector<float> src, std::vector<float> dest) {
   vector<vector<float>> path;
 
-	Vector3D srcPosition  = Vector3D(src); // current position of the drone
-	Vector3D destPosition = Vector3D(dest);
-	Vector3D nextPosition = srcPosition; // sets to the current position, for now
-	Vector3D flyToPosition;
+  Vector3D srcPosition = Vector3D(src);   // current position of the drone
+  Vector3D destPosition = Vector3D(dest); // destination position of the drone
+  Vector3D flyToPosition;
 
-	bool hasFlownUp 		    = false;
-	bool hasFlownHorizontal = false;
-	bool hasFlownDown 		  = false;
-	float heightToAvoidCollision = 400;
+  float heightToAvoidCollision = 400;     // height where the drone must not touch any building
 
-	while (true) {
-		if (!hasFlownUp) {
-			if (nextPosition.GetY() >= heightToAvoidCollision) {
-				// successfully moved up
-				hasFlownUp = true;
-			} else { 
-				// move current position up until heightToAvoidCollision 
-				flyToPosition = nextPosition;
-				flyToPosition.SetY(heightToAvoidCollision);
-			}
-		}
+  // Go up to heightToAvoidCollision from the source location
+  flyToPosition = srcPosition;
+  flyToPosition.SetY(heightToAvoidCollision);
+  path.push_back(toVectorFloat(flyToPosition));
 
-		if (hasFlownUp && !hasFlownHorizontal) {
-			if (nextPosition.IsWithinXandZRange(destPosition, 3)) {
-				// successfully flown to destination position (with y = heightToAvoidCollision)
-				hasFlownHorizontal = true;
-			} else {
-				// move from source position (with y = heightToAvoidCollision)
-				// to destination position (with y = heightToAvoidCollision)
-				flyToPosition = destPosition;
-				flyToPosition.SetY(heightToAvoidCollision);
-			}
-		}
+  // Go horizontally to the destination location
+  flyToPosition = destPosition;
+  flyToPosition.SetY(heightToAvoidCollision);
+  path.push_back(toVectorFloat(flyToPosition));
 
-		if (hasFlownHorizontal && !hasFlownDown) {
-			if (Distance(nextPosition, destPosition) <= 5) {
-				// successfully flown to destination position
-				hasFlownDown = true;
-			} else {
-				// move toward the destination
-				flyToPosition = destPosition;
-			}
-		}
+  // Go down to reach the source location
+  flyToPosition = destPosition;
+  path.push_back(toVectorFloat(flyToPosition));
 
-		// calculate the direction
-		Vector3D direction = flyToPosition - nextPosition;
-
-		// normalize the direction
-		direction = direction.Normalize();
-
-		// calculate the velocity of the drone
-		Vector3D velocity = direction * droneSpeed;
-
-		// calculate the distance traveled over this time step: s = v*dt
-		nextPosition = nextPosition + velocity * (dt * 10);
-
-		path.push_back(toVectorFloat(nextPosition));
-
-		if (hasFlownDown) {
-			return path;
-		}
-	}
+  return path;
 }
 
-} // close namespace csci3081
+}  // namespace csci3081
