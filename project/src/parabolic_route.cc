aefef9f360da693c294f9b6992babb5c456d481c
Aunya Mukherjee <mukhe074@umn.edu>
2021-04-13 19:15:27 -0500

    feature(Routes): made beeline, implemented placeholder - created beeline class - set all routes to smart path as a placeholder - #42

--- project/src/parabolic_route.cc
+++ project/src/parabolic_route.cc
@@ -3,6 +3,6 @@
 namespace csci3081 {
     std::vector <std::vector<float>> ParabolicRoute::GetRoute(IGraph* graph, std::vector<float> location, std::vector<float> dest){
         //Logic for generating the parabolic path points goes here
-        return {{}};
+        return graph->GetPath(location, dest);
     }
 }
