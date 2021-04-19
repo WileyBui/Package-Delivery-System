2d12884d1f473860de0ad1106bc53ce847b8b215
xiaoxuan <zhan5472@umn.edu>
2021-04-19 17:58:19 -0500

    feature(parabolic_route.h):add comment

--- project/src/parabolic_route.cc
+++ project/src/parabolic_route.cc
@@ -7,19 +7,18 @@ namespace csci3081 {
         Vector3D start(location);
         Vector3D end(dest);
         Vector3D middle = (start + end)/2;
-        Vector3D dV = (end - start)/steps;
+        Vector3D dV = (end - start)/steps;//delta Vector
         float dis_mid_source = Distance(start,middle);
-        // float middle = Distance(start,end);
         std::vector <std::vector<float>> path;
         Vector3D current = start;
         path.push_back(location);
         for(int i = 0;i < steps-1; i++){
-            current = current + dV;
+            current = current + dV;//update the current position 
             std::vector<float> converted = toVectorFloat(current);
-            float dis_mid_cur = Distance(middle,current);
-            float dY = (1 - (dis_mid_cur*dis_mid_cur)/(dis_mid_source*dis_mid_source))*j;
+            float dis_mid_cur = Distance(middle,current);//calculate the distance between current position to the middle point
+            float dY = (1 - (dis_mid_cur*dis_mid_cur)/(dis_mid_source*dis_mid_source))*j;//caculate the delta height that using the forumla from lab14 
             converted[1]+=dY;
-            path.push_back(converted);
+            path.push_back(converted);//add point to the path
         }
         path.push_back(dest);
         return path;
